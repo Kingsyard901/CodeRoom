fe5d891aa1e70992a9449897c51138120f7c4114
Kingsyard901 <henrik.ekelund@outlook.com>
2021-08-15 08:27:29 +0200

    Disable Image Gallery
    Keeping the code, but comment out, will use other solutions to show projects.

--- script.js
+++ script.js
@@ -33,74 +33,34 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-
-// START Script Image Gallery OLD VERSION
-
-// function openModal() {
-//   document.getElementById("myModal").style.display = "block";
-// }
-
-// function closeModal() {
-//   document.getElementById("myModal").style.display = "none";
-// }
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("demo");
-//   var captionText = document.getElementById("caption");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-//   captionText.innerHTML = dots[slideIndex-1].alt;
-// }
-
-// END Script Image Gallery OLD VERSION
-
-
 // Script Image Gallery New Version
-const gallery = document.getElementById('gallery');
-const popup = document.getElementById('popup');
-const selectedImage = document.getElementById('selectedImage');
+// Use below code for regular image gallery if wanted.
 
-const imageIndexes = [1,2,3,4,5];
-const selectedIndex = null;
+// const gallery = document.getElementById('gallery');
+// const popup = document.getElementById('popup');
+// const selectedImage = document.getElementById('selectedImage');
 
-imageIndexes.forEach((i) => {
-    const image = document.createElement('img');
-    image.src = `/img/projectImg/cover_episode-${i}.jpg`;
-    image.alt = `Cover for episode ${i} och the project`;
-    image.classList.add('galleryImg');
+// const imageIndexes = [1,2,3,4,5];
+// const selectedIndex = null;
 
-    image.addEventListener('click', () => {
-        // Popup stuff
-        popup.style.transform = `translateY(0)`;
-        selectedImage.src = `/img/projectImg/cover_episode-${i}.jpg`;
-        selectedImage.alt = `Cover for episode ${i} och the project`;
-    });
-    gallery.appendChild(image);
-});
+// imageIndexes.forEach((i) => {
+//     const image = document.createElement('img');
+//     image.src = `/img/projectImg/cover_episode-${i}.jpg`;
+//     image.alt = `Cover for episode ${i} och the project`;
+//     image.classList.add('galleryImg');
 
-popup.addEventListener('click', () => {
-    popup.style.transform = `translateY(-100%)`;
-    popup.src = '';
-    popup.alt = '';
-});
+//     image.addEventListener('click', () => {
+//         // Popup stuff
+//         popup.style.transform = `translateY(0)`;
+//         selectedImage.src = `/img/projectImg/cover_episode-${i}.jpg`;
+//         selectedImage.alt = `Cover for episode ${i} och the project`;
+//     });
+//     gallery.appendChild(image);
+// });
+
+// popup.addEventListener('click', () => {
+//     popup.style.transform = `translateY(-100%)`;
+//     popup.src = '';
+//     popup.alt = '';
+// });
+// END OF regular image gallery code.
